ceb972f7a2ba790c698f41c8a5bdd23c5e365f93
master <lvyexuwenfa100@126.com>
2016-01-07 16:41:54 +0800

    test

--- dhub/models/music.py
+++ dhub/models/music.py
@@ -3,9 +3,6 @@ __author__ = 'starnet'
 from model import BaseModel
 
 class Music(BaseModel):
-    url=''
+    url='http://www.baidu.com'
     def __init__(self, url):
         self.url = url
-
-    def get(self):
-        return Music('http://www.baidu.com').tojson()
